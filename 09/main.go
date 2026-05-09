e272f4dfa94a58b43420d6a15c898fbc3fd9c925
IseAkira <akira.ise.m@gmail.com>
2026-05-09 18:51:25 +0900

    go routine

---
+++ 09/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i<10; i ++{
+		v := i
+		wg.Add(1)
+		go func(){
+			defer wg.Done()
+			fmt.Println(v)
+		}()
+	}
+	wg.Wait()
+}

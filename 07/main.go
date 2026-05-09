2f9bdba218f509e913b1918634787082836c3fdd
IseAkira <akira.ise.m@gmail.com>
2026-05-09 18:34:30 +0900

    07終了

---
+++ 07/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"time"
+)
+
+func sendMessage(msg string) {
+	println(msg)
+}
+
+func main() {
+	message := "hi"
+	go func() {
+		sendMessage(message)
+	}()
+
+	message = "ho"
+
+	time.Sleep(time.Second)
+	println(message)
+	time.Sleep(time.Second)
+}
+
+
+
+

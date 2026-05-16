a3f27a2fae581eb8f3381f9f3cdc7f8e4a0d0453
IseAkira <akira.ise.m@gmail.com>
2026-05-16 21:49:38 +0900

    簡単なサーバーが作成

--- 14/main.go
+++ 14/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func main() {
-	fmt.Println("%s %s","hello")
+func main(){
+	http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
+		fmt.Fprintf(w,"Hello World!")
+	})
+	http.ListenAndServe(":8080",nil)
 }
